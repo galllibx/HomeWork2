6776be97c47e3390a034dd77d86871327d8343a2
galibx <pswxwq@gmail.com>
2022-08-31 11:34:08 +0700

    post

---
+++ post.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h1 align="center">Webboard GUBGIB</h1>
+    <hr>
+    <div>
+    <center><?php echo "ต้องการกระทู้หมายเลข $_GET[id]"; ?></center>
+    </div>
+    <br>
+   
+    <table style="border: 2px solid black; width: 40%;" align="center">
+        <tr><td style="background-color: #6CD2FE;" colspan="2">แสดงความคิดเห็น</td></tr>
+        <tr><td><textarea name="" id="" cols="100" rows="10"></textarea></td></tr>
+        <tr align='center'><td><input type="submit"value="message"></td></tr>
+
+    </table>
+    <div align="center"><a href="homework2.html">กลับไปหน้าหลัก</a></div>
+    
+</body>
+</html>
---
+++ verify.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Verify</title>
+</head>
+<body>
+    <h1 align="center">Webboard GUBGIB</h1>
+    <hr>
+    <div align ="center">
+        เข้าสู่ระบบด้วย <br>
+        Login = <?php echo $_POST["login"]; ?>
+        <br>
+        Password = <?php echo $_POST["pwd"]; ?>
+    </div>
+
+</body>
+</html>
